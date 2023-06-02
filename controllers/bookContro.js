419fb6e56bfa2b5a98382a72a51305bc64af5d7d
nirapadak <naminopak@gmail.com>
2023-06-02 10:35:02 +0600

    singal book Get ERROR update

--- controllers/bookContro.js
+++ controllers/bookContro.js
@@ -30,34 +30,84 @@ exports.findBook = async (req, res) => {
     })
   }
 }
-//retrieve a book ----------------------
-exports.updateBook = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updateBook = await Book.findByIdAndUpdate(
-      {
-        _id: id,
-      },
-      {
-        $set: {
-          title: res.body.title,
-          author: res.body.author,
-          description: res.body.description,
-          publishedYear: res.body.publishedYear
+
+
+// GET One book from the database -----------
+exports.oneBookGet = async(req,res)=>{
+    try {
+       const {id} = req.params;
+       const singleBook = await Book.findOne({_id:id});
+        res.status(200).json(
+            {
+                success: true,
+                message:"single book get done",
+                Data:singleBook
+            }
+        );
+    } catch (error) {
+        res.status(404).json(
+            {
+                error:error.message
+            }
+        )
+    }
+};
+
+
+// update a book -----------------
+exports.singleBookUpdate = async(req,res)=>{
+    try {
+       const {id} = req.params;
+       const singleBookUpdate = await Book.findByIdAndUpdate(
+        {
+            _id: id
+        },
+        {
+            $set:{
+                title:req.body.title,
+                author:req.body.author,
+                description:req.body.description,
+                publishedYear:req.body.publishedYear
+            }
+        },
+        {
+            new: true
         }
-      },
-      {
-        new: true
-      }
-    )
-    res.status(200).json({
-      "msg": "Updated successfully",
-      data:updateBook
-    })
-  } catch (error) {
-    res.status(404).json({
-      "msg": "Error updating book",
-      "error": error.message
-    })
-  }
-}
+       );
+        res.status(200).json(
+            {
+                success: true,
+                message:"single book update done",
+                Data:singleBookUpdate
+            }
+        );
+    } catch (error) {
+        res.status(404).json(
+            {
+                error:error.message
+            }
+        )
+    }
+};
+
+
+// delete a book from the database ----------
+exports.singleBookDelete = async(req,res)=>{
+    try {
+       const {id} = req.params;
+       const singleBookDelete = await Book.findByIdAndDelete({ _id: id });
+        res.status(200).json(
+            {
+                success: true,
+                message:"single book delete done",
+                Data:singleBookDelete
+            }
+        );
+    } catch (error) {
+        res.status(404).json(
+            {
+                error:error.message
+            }
+        )
+    }
+};
--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const routes = express.Router();
 
-const { createBook, findBook, updateBook} = require('../controllers/bookContro');
+const { createBook, findBook, oneBookGet, singleBookUpdate, singleBookDelete} = require('../controllers/bookContro');
 
 
 routes.post('/books', createBook);
-routes.get('/booksOne', findBook);
-routes.put('/books/:id', updateBook);
+routes.get('/books', findBook);
+routes.get('/books/:id', oneBookGet);
+routes.put('/books/:id', singleBookUpdate);
+routes.delete('/books/:id', singleBookDelete);
+
 
 
 module.exports = routes
